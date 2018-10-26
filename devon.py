10def72279b9a935f05d46bc2bb045211d833dee
DevonChen42 <31545136+DevonChen42@users.noreply.github.com>
2018-10-26 09:16:22 -0700

    Add files via upload

--- devon.py
+++ devon.py
@@ -1,20 +1,20 @@
 
 from turtle import *
 from random import randint
-t = Turtle()
-t.bgcolor('black')
+
+bgcolor('black')
 x = 1
-t.speed(0)
+speed(0)
 while x < 500:
 
 	r = randint(0,255)
 	g = randint(0,255)
 	b = randint(0,255)
 
-	t.colormode(255)
-	t.pencolor(r,g,b)
-	t.fd(50 + x)
-	t.rt(90.911)
-	x = x+1
-
+	colormode(255)
+	pencolor(r,g,b)
+	fd(50 + x)
+	rt(90.911)
+	x = x-1
+	print(x,r,g,b)
 exitonclick()

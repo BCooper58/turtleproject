224ce881190a1fa88bdbda4f8405e9a1dcf1ee69
S3thBr0wn <31544947+S3thBr0wn@users.noreply.github.com>
2018-10-26 09:59:12 -0700

    Add files via upload

--- seth.py
+++ seth.py
@@ -1,11 +1,14 @@
 from turtle import *
 import turtle
-color('blue' , 'red')
-begin_fill()
-while True:
-    right(200)
-    forward(170)
-    if abs(pos()) < 1:
-        break
-end_fill()
-exitonclick()
+speed(0)
+def Seth():
+    color('black' , 'green')
+    begin_fill()
+    while True:
+        right(250)
+        forward(270)
+        if abs(pos()) < 1:
+            break
+    end_fill()
+    exitonclick()
+Seth()
